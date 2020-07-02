35c09022ecd6b4560ef21c0015ab9f44ba68c5ec
juncid <juncid@gmail.com>
2020-07-02 17:42:24 -0400

    Beta update endpoint /movies

--- movies_api/app.py
+++ movies_api/app.py
@@ -5,6 +5,8 @@ from flask import request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
+from marshmallow import ValidationError
+
 app = FlaskLambda(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
@@ -32,20 +34,11 @@ class Movie(db.Model):
     language = db.Column(db.String(300))
     runtime= db.Column(db.String(10))
 
-    def __init__(self, title, year, age):
-        self.title = title
-        self.year = year
-        self.age = age
 
 
-class MovieSchema(ma.Schema):
+class MovieSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
-        fields = (
-            'title',
-            'year',
-            'age',
-            'runtime'
-        )
+        model = Movie
 
 
 
@@ -60,6 +53,53 @@ def index():
 
 @app.route("/movie/list", methods=['GET'])
 def get_movies():
-    all_movies = Movie.query.all()
+    all_movies = Movie.query.order_by(Movie.title).all()
     result = movies_schema.dump(all_movies)
     return jsonify(result)
+
+
+@app.route("/movie", methods=['POST', 'PUT', 'DELETE'])
+def create_update_movie():
+
+    if request.method == 'POST':
+        data = request.get_json()
+
+        return jsonify(data)
+
+    elif request.method == 'PUT': 
+        data = request.get_json()
+
+        movie_to_update = Movie.query.get(data['id'])
+
+        movie_to_update.title = data['title']
+        movie_to_update.year = data['year']
+        movie_to_update.age = data['age']
+        movie_to_update.imbd = data['imbd']
+        movie_to_update.rotten_tomatoes = data['rotten_tomatoes']
+        movie_to_update.netflix = data['netflix']
+        movie_to_update.hulu = data['hulu']
+        movie_to_update.prime_video = data['prime_video']
+        movie_to_update.disney_plus = data['disney_plus']
+        movie_to_update.movie_type = data['movie_type']
+        movie_to_update.directors = data['directors']
+        movie_to_update.genres = data['genres']
+        movie_to_update.country = data['country']
+        movie_to_update.language = data['language']
+        movie_to_update.runtime = data['runtime']
+
+        db.session.commit()
+
+        result = movie_schema.dump(movie_to_update)
+
+        return jsonify(result)
+
+    elif request.method == 'DELETE':
+
+        data = request.args.get('id')
+        movie = Movie.query.get(data)
+
+        result = movie_schema.dump(movie)
+
+        return jsonify(result)
+        
+    
