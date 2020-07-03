458886e467f5c5436eeb43b718de92a2fd981bf2
juncid <juncid@gmail.com>
2020-07-02 22:40:10 -0400

    Add verb Post and Put for endpoint /movies. Add serializers for /movies and /movies/list endpoint

--- movies_api/app.py
+++ movies_api/app.py
@@ -4,13 +4,14 @@ from flask_lambda import FlaskLambda
 from flask import request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from marshmallow import ValidationError, Schema, fields
 
-from marshmallow import ValidationError
 
 app = FlaskLambda(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+app.config['JSON_SORT_KEYS']=False
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
@@ -36,14 +37,24 @@ class Movie(db.Model):
 
 
 
-class MovieSchema(ma.SQLAlchemyAutoSchema):
-    class Meta:
-        model = Movie
-
-
-
-movie_schema = MovieSchema()
-movies_schema = MovieSchema(many=True)
+class MovieSchema(Schema):
+    id = fields.Integer(required=False)
+    title = fields.String(required=True)
+    year = fields.String(required=True)
+    age = fields.String(required=True)
+    imbd = fields.String(required=True)
+    rotten_tomatoes = fields.String(required=True)
+    netflix = fields.String(required=True)
+    hulu = fields.String(required=True)
+    prime_video = fields.String(required=True)
+    disney_plus = fields.String(required=True)
+    movie_type = fields.String(required=True)
+    directors = fields.String(required=True)
+    genres = fields.String(required=True)
+    country = fields.String(required=True)
+    language = fields.String(required=True)
+    runtime= fields.String(required=True)
+    
 
 @app.route("/hello")
 def index():
@@ -53,6 +64,7 @@ def index():
 
 @app.route("/movie/list", methods=['GET'])
 def get_movies():
+    movies_schema = MovieSchema(many=True)
     all_movies = Movie.query.order_by(Movie.title).all()
     result = movies_schema.dump(all_movies)
     return jsonify(result)
@@ -61,44 +73,76 @@ def get_movies():
 @app.route("/movie", methods=['POST', 'PUT', 'DELETE'])
 def create_update_movie():
 
+    movie_schema = MovieSchema()
+
     if request.method == 'POST':
+        
         data = request.get_json()
 
-        return jsonify(data)
+        try:
+            movie_schema.load(data)
+        except ValidationError as err:
+            return jsonify(err.messages), 404
+
+        movie = Movie()
+        movie = initialize_movie(movie, data)
+
+        db.session.add(movie)
+        db.session.commit()
+
+        result = movie_schema.dump(movie)
+
+        return jsonify(result), 201
 
     elif request.method == 'PUT': 
         data = request.get_json()
 
-        movie_to_update = Movie.query.get(data['id'])
+        try:
+            movie_schema.load(data)
+        except ValidationError as err:
+            return jsonify(err.messages), 404
 
-        movie_to_update.title = data['title']
-        movie_to_update.year = data['year']
-        movie_to_update.age = data['age']
-        movie_to_update.imbd = data['imbd']
-        movie_to_update.rotten_tomatoes = data['rotten_tomatoes']
-        movie_to_update.netflix = data['netflix']
-        movie_to_update.hulu = data['hulu']
-        movie_to_update.prime_video = data['prime_video']
-        movie_to_update.disney_plus = data['disney_plus']
-        movie_to_update.movie_type = data['movie_type']
-        movie_to_update.directors = data['directors']
-        movie_to_update.genres = data['genres']
-        movie_to_update.country = data['country']
-        movie_to_update.language = data['language']
-        movie_to_update.runtime = data['runtime']
+        movie_to_update = Movie.query.get_or_404(data['id'])
+
+        if not movie_to_update:
+            result = movie_schema.dump(movie_to_update)
+            return jsonify(result)
+
+        
+        movie = initialize_movie(movie_to_update, data)
 
         db.session.commit()
 
-        result = movie_schema.dump(movie_to_update)
+        result = movie_schema.dump(movie)
 
         return jsonify(result), 201
 
     elif request.method == 'DELETE':
 
         id_movie = request.args.get('id')
-        movie = Movie.query.get(id_movie)
+        movie = Movie.query.get_or_404(id_movie)
 
         db.session.delete(movie)
-        db.session.commit
+        db.session.commit()
         
         return jsonify({"message": "Movie Deleted!"}), 202
+
+
+def initialize_movie(movie, data):
+        movie.title = data['title']
+        movie.year = data['year']
+        movie.age = data['age']
+        movie.imbd = data['imbd']
+        movie.rotten_tomatoes = data['rotten_tomatoes']
+        movie.netflix = data['netflix']
+        movie.hulu = data['hulu']
+        movie.prime_video = data['prime_video']
+        movie.disney_plus = data['disney_plus']
+        movie.movie_type = data['movie_type']
+        movie.directors = data['directors']
+        movie.genres = data['genres']
+        movie.country = data['country']
+        movie.language = data['language']
+        movie.runtime = data['runtime']
+
+        return movie
